0e394f46dd6d5927e8b26de140af56bc5b4593d1
passion-dev37 <passion.dev37@outlook.com>
2019-12-27 21:29:26 +1300

    added some template pages from MUI

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,16 @@
-import React, { Component } from 'react';
-import AppNavbar from './components/AppNavbar';
-import ShoppingList from './components/ShoppingList';
-import ItemModal from './components/ItemModal';
-import { Container } from 'reactstrap';
+import React, { Component } from "react";
+// import AppNavbar from './components/AppNavbar';
+// import ShoppingList from './components/ShoppingList';
+// import ItemModal from './components/ItemModal';
+// import { Container } from 'reactstrap';
 
-import { Provider } from 'react-redux';
-import store from './store';
-import { loadUser } from './actions/authActions';
+import { Provider } from "react-redux";
+import store from "./store";
+import { loadUser } from "./actions/authActions";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import Dashboard from "./components/dashboard/Dashboard";
 
 class App extends Component {
   componentDidMount() {
@@ -19,13 +20,14 @@ class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <div className='App'>
+        {/* <div className='App'>
           <AppNavbar />
           <Container>
             <ItemModal />
             <ShoppingList />
           </Container>
-        </div>
+        </div> */}
+        <Dashboard />
       </Provider>
     );
   }
