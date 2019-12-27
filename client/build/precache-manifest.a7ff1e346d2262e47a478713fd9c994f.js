db1a0be54d9f28df600c735d93c348442f8ac3e3
passion-dev37 <passion.dev37@outlook.com>
2019-12-27 13:54:28 +1300

    completed a simple backend and frontend for my portfolio

---
+++ client/build/precache-manifest.a7ff1e346d2262e47a478713fd9c994f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5a38d83ea65cea33d1d8300369efe60",
+    "url": "/index.html"
+  },
+  {
+    "revision": "abb780adf76cfcb5c984",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "4cbe0290bbc10b160b44",
+    "url": "/static/css/main.f0019100.chunk.css"
+  },
+  {
+    "revision": "abb780adf76cfcb5c984",
+    "url": "/static/js/2.f9bb12c9.chunk.js"
+  },
+  {
+    "revision": "87984cad984dae3222b2a1d0861003a4",
+    "url": "/static/js/2.f9bb12c9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4cbe0290bbc10b160b44",
+    "url": "/static/js/main.c9cbead9.chunk.js"
+  },
+  {
+    "revision": "6b3defdcd169e5f902d5",
+    "url": "/static/js/runtime-main.1dc66141.js"
+  }
+]);
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://Mark-Zhu:Mark19960630@myportfolio-mjznb.mongodb.net/test?retryWrites=true&w=majority',
+}
