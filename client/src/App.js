33e649731c682106417916b6cc624ad9b2a8af37
passion-dev37 <passion.dev37@outlook.com>
2019-12-28 21:00:13 +1300

    removed album.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
-// import AppNavbar from './components/AppNavbar';
-// import ShoppingList from './components/ShoppingList';
-// import ItemModal from './components/ItemModal';
-// import { Container } from 'reactstrap';
+import AppNavbar from './components/AppNavbar';
+import ShoppingList from './components/ShoppingList';
+import ItemModal from './components/ItemModal';
+import { Container } from 'reactstrap';
 
 import { Provider } from "react-redux";
 import store from "./store";
@@ -10,7 +10,6 @@ import { loadUser } from "./actions/authActions";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import Dashboard from "./components/dashboard/Dashboard";
 
 class App extends Component {
   componentDidMount() {
@@ -20,14 +19,13 @@ class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        {/* <div className='App'>
+        <div className='App'>
           <AppNavbar />
           <Container>
             <ItemModal />
             <ShoppingList />
           </Container>
-        </div> */}
-        <Dashboard />
+        </div>
       </Provider>
     );
   }
