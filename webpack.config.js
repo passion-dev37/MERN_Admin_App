51ea94a772a87d2ac4bb8cd530eccd0ed8e0f616
passion-dev37 <passion.dev37@outlook.com>
2020-05-15 17:57:13 +1200

    added function to get all filePaths recursively

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
-const path = require('path');
 const globule = require('globule');
+const { readdirSync } = require('fs')
+const path = require( 'path' );
 
-
+const filePaths = recursiveFileSearch(,globule.find("./wwwroot/js/" + ".js"));
 const jsFilePaths = globule.find("./wwwroot/js/" + "*.js")
-    .filter(filePath => filePath.indexOf("es5.js") === -1 || filePath.indexOf("min.js") === -1) ; // filter out compiled files.
+    .filter(filePath => filePath.indexOf("es5.js") === -1 && filePath.indexOf("min.js") === -1) ; // filter out compiled files.
 module.exports = {
     entry: jsFilePaths,
     output: {
@@ -12,3 +13,20 @@ module.exports = {
     },
     mode: 'none'
 };
+
+
+/**
+ * recursively search all subfolders in a given directory for a specified type of file 
+ * and return the found filePaths as an array.
+ * 
+ * rootDirectory: The root directory to start searching in.
+ */
+async function recursiveFileSearch(rootDirectory, filePaths, fileSuffix) {
+    
+
+    readdirSync(source, { withFileTypes: true })
+        .filter(dirent => dirent.isDirectory()).forEach(directory => {
+            
+    })
+}
+//https://stackoverflow.com/questions/18112204/get-all-directories-within-directory-nodejs/24594123
