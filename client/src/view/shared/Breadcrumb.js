cc78049cc3216521054f8ef771cb72488e241483
passion-dev37 <passion.dev37@outlook.com>
2020-06-04 12:18:16 +1200

    added auto fix eslint problems on save

--- client/src/view/shared/Breadcrumb.js
+++ client/src/view/shared/Breadcrumb.js
@@ -1,21 +1,28 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import { Breadcrumb as AntBreadcrumb } from "antd";
-import Container from "@material-ui/core/Container";
+import React, {Component} from 'react';
 
+import {Breadcrumb as AntBreadcrumb} from 'antd';
+import Container from '@material-ui/core/Container';
+
+/**
+ * breadscrumb to provide navigation information on each page.
+ */
 class Breadcrumb extends Component {
+  /**
+   *
+   * @return {*}
+   */
   render() {
     return (
       <Container
         style={{
-          padding: "20px",
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center"
+          padding: '20px',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
         }}
       >
         <AntBreadcrumb>
-          {this.props.items.map(item => (
+          {this.props.items.map((item) => (
             <AntBreadcrumb.Item key={item[0]}>{item[0]}</AntBreadcrumb.Item>
           ))}
         </AntBreadcrumb>
