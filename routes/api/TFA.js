e8664a26d4d3dbe81d67876ba08a3d5afaa44abd
passion-dev37 <passion.dev37@outlook.com>
2020-06-16 23:17:15 +1200

    added eslint and associated it with prettier. Now prettier formats code on save. added husky and lint-staged to auto fix all eslint problems before commit. removed client eslintrc.json

--- routes/api/TFA.js
+++ routes/api/TFA.js
@@ -1,120 +1,120 @@
-//google two-factor authenticator.
-const express = require("express");
+// google two-factor authenticator.
+const express = require('express');
 const router = express.Router();
-const speakeasy = require("speakeasy");
-const QRCode = require("qrcode");
-const TFA = require("../../models/TFA");
-const userModels = require("../../models/User");
-const auth = require("../../middleware/auth");
+const speakeasy = require('speakeasy');
+const QRCode = require('qrcode');
+const TFA = require('../../models/TFA');
+const userModels = require('../../models/User');
 
 // @route   POST api/TFA/setup
 // @desc    Get setup TFA for the user
 // @access  Private
-router.post("/", (req, res) => {
-  const { email } = req.body;
-  TFA.findOne({ email })
-    .then((TFA) => {
-      if (!TFA)
-        return res.status(400).json({ msg: "TFA document Does not exist" });
-      res.send(TFA);
-    })
-    .catch((err) => res.status(400).json({ msg: err }));
+router.post('/', (req, res) => {
+  const {email} = req.body;
+  TFA.findOne({email})
+      .then((TFA) => {
+        if (!TFA) {
+          return res.status(400).json({msg: 'TFA document Does not exist'});
+        }
+        res.send(TFA);
+      })
+      .catch((err) => res.status(400).json({msg: err}));
 });
 
 // @route   POST api/TFA/setup
 // @desc    construct TFA setup and save it to db for the user
 // @access  Private
-router.post("/setup", (req, res) => {
-  const { email, domainName, isOauth, uniqueId } = req.body;
+router.post('/setup', (req, res) => {
+  const {email, domainName, isOauth, uniqueId} = req.body;
 
-  let lookup = isOauth ? uniqueId : email;
-  userModels.User.findOne({ lookup })
-    .then((user) => {
-      if (!user) {
-        //return 400 if user does not even exist.
-        return res.status(400).json({ msg: "user does not even exist!" });
-      }
-    })
-    .catch((err) => res.status(400).json({ msg: err }));
+  const lookup = isOauth ? uniqueId : email;
+  userModels.User.findOne({lookup})
+      .then((user) => {
+        if (!user) {
+        // return 400 if user does not even exist.
+          return res.status(400).json({msg: 'user does not even exist!'});
+        }
+      })
+      .catch((err) => res.status(400).json({msg: err}));
 
-  TFA.findOne({ email })
-    .then((TFADoc) => {
-      if (TFADoc) return res.status(400).json({ msg: "TFA already exists" });
-      //if tfa does not exist, create a new one and send it out as response.
-      const secret = speakeasy.generateSecret({
-        length: 10,
-        name: email,
-        issuer: "My Admin App",
-      });
-      var url = speakeasy.otpauthURL({
-        secret: secret.base32,
-        label: email,
-        issuer: domainName,
-        encoding: "base32",
-      });
-      QRCode.toDataURL(url, (err, dataURL) => {
-        const newTFA = new TFA({
-          secret: secret.base32,
-          dataURL: dataURL,
-          TFAURL: url,
-          email: email,
+  TFA.findOne({email})
+      .then((TFADoc) => {
+        if (TFADoc) return res.status(400).json({msg: 'TFA already exists'});
+        // if tfa does not exist, create a new one and send it out as response.
+        const secret = speakeasy.generateSecret({
+          length: 10,
+          name: email,
+          issuer: 'My Admin App',
         });
-        newTFA
-          .save()
-          .then((data) => {
-            TFA.findOne({ email }).then((TFADoc) => res.send(TFADoc));
-          })
-          .catch((err) => res.status(400).json({ msg: err }));
-      });
-    })
-    .catch((err) => res.status(400).json({ msg: err }));
+        const url = speakeasy.otpauthURL({
+          secret: secret.base32,
+          label: email,
+          issuer: domainName,
+          encoding: 'base32',
+        });
+        QRCode.toDataURL(url, (err, dataURL) => {
+          const newTFA = new TFA({
+            secret: secret.base32,
+            dataURL: dataURL,
+            TFAURL: url,
+            email: email,
+          });
+          newTFA
+              .save()
+              .then((data) => {
+                TFA.findOne({email}).then((TFADoc) => res.send(TFADoc));
+              })
+              .catch((err) => res.status(400).json({msg: err}));
+        });
+      })
+      .catch((err) => res.status(400).json({msg: err}));
 });
 
 // @route   DELETE api/TFA/setup
 // @desc    Delete a TFA document
 // @access  Private
-router.delete("/setup", (req, res) => {
-  const { email } = req.body;
+router.delete('/setup', (req, res) => {
+  const {email} = req.body;
 
-  TFA.findOne({ email })
-    .then((TFA) => TFA.remove().then(() => res.json({ success: true })))
-    .catch((err) => res.status(404).json({ success: false, msg: err }));
+  TFA.findOne({email})
+      .then((TFA) => TFA.remove().then(() => res.json({success: true})))
+      .catch((err) => res.status(404).json({success: false, msg: err}));
 });
 
 // @route   POST api/TFA/verify
 // @desc    verify TFA.
 // @access  Private
-router.post("/verify", (req, res) => {
-  const { email, code, TFA } = req.body;
+router.post('/verify', (req, res) => {
+  const {email, code, TFA} = req.body;
   // var token = speakeasy.totp({
   //   secret: TFA.secret,
   //   encoding: "base32"
   // });
 
-  TFA.findOne({ email })
-    .then((TFA) => {
-      let isVerified = speakeasy.totp.verify({
-        secret: TFA.secret,
-        encoding: "base32",
-        token: code,
-      });
+  TFA.findOne({email})
+      .then((TFA) => {
+        const isVerified = speakeasy.totp.verify({
+          secret: TFA.secret,
+          encoding: 'base32',
+          token: code,
+        });
 
-      if (isVerified) {
-        return res.status(200).json({
-          msg: "verification successfull",
+        if (isVerified) {
+          return res.status(200).json({
+            msg: 'verification successfull',
+          });
+        } else {
+          return res.status(400).json({
+            msg:
+            'verification unsuccessfull. Probably because wrong code is provided',
+          });
+        }
+      })
+      .catch((err) => {
+        res.status(403).json({
+          msg: err,
         });
-      } else {
-        return res.status(400).json({
-          msg:
-            "verification unsuccessfull. Probably because wrong code is provided",
-        });
-      }
-    })
-    .catch((err) => {
-      res.status(403).json({
-        msg: err,
       });
-    });
 });
 
 module.exports = router;
--- routes/api/admin.js
+++ routes/api/admin.js
@@ -1,15 +1,14 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-
-const Log = require("../../models/Log");
+const Log = require('../../models/Log');
 
 // @route   GET api/admin/logs
 // @desc    Get All logs Users
 // @access  Public
-router.get("/logs", (req, res) => {
+router.get('/logs', (req, res) => {
   Log.find()
-    .sort({ date_logged: -1 })
-    .then((logs) => res.json(logs));
+      .sort({date_logged: -1})
+      .then((logs) => res.json(logs));
 });
 
 module.exports = router;
